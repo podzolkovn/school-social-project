b237d90b35ddd3bbb52e6d0ac4854131c6f67c36
Джена Абишева <jenetka245@gmail.com>
2019-02-02 15:19:05 +0600

    Подключила Rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190131123253) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "second_name"
-    t.string "birthday_day"
-    t.string "school"
-    t.string "grade"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema.define(version: 20_190_131_123_253) do
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'second_name'
+    t.string 'birthday_day'
+    t.string 'school'
+    t.string 'grade'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
