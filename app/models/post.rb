2d3c1c2eff70d9227fbc15c28dd58c6f8d621915
Джена Абишева <jenetka245@gmail.com>
2019-02-03 01:17:35 +0600

    Модель Post

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ApplicationRecord
+	  validates :description, presence: true, length: { in: 5..500 }
+  # validate :correct_image_type
+
+  belongs_to :user
+
+  has_one_attached :picture, dependent: :destroy
+
+  has_one_attached :attachment
+end
