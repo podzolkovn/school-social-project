b625c62d2359aef01d3e200012561786aaa16a03
Джена Абишева <jenetka245@gmail.com>
2019-02-03 00:02:43 +0600

    Добавила Active_Admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+	def access_denied(exception)
+	  flash[:danger] = exception.message
+  	  redirect_to root_url
+	end
 end
